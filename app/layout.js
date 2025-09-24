c1597838c4bdf9ec4348dd6657a6ecc512f32105
Saulo Valin <seu-email@exemplo.com>
2025-09-24 10:08:59 -0300

    Google

--- app/layout.js
+++ app/layout.js
@@ -27,6 +27,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+        {/* SEO/Redes sociais */}
+        <meta name="robots" content="index, follow" />
+        <meta property="og:title" content="DNA de Vendas Imobiliárias" />
+        <meta property="og:description" content="Imersão para alta performance no mercado imobiliário" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://dnadeperformance.com.br/" />
+        <meta property="og:image" content="http://dnadeperformance.com.br/DNA-FEED (1).png" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </head>
       <body
         className={`${bioRhyme.variable} ${chivo.variable} ${montserrat.variable} antialiased`}
       >
